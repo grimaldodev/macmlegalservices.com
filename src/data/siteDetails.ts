91898663edded08bad7aac5e42986e57d5629bc1
Ivan Grimaldo <ivan@grimaldo.dev>
2025-02-24 21:53:06 -0600

    Coming soon message

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,12 +1,12 @@
 export const siteDetails = {
-    siteName: 'Finwise',
-    siteUrl: 'https://finwise-omega.vercel.app/',
+    siteName: 'MACM Legal Services',
+    siteUrl: 'https://macmlegalservices.com/',
     metadata: {
-        title: 'Finwise - Next.js and Tailwind CSS Landing Page Template',
-        description: 'Finwise empowers businesses with cutting-edge technology solutions to drive success and efficiency.',
+        title: 'MACM Legal Services',
+        description: 'Legal services in Puerto Vallarta Mexico',
     },
     language: 'en-us',
     locale: 'en-US',
     siteLogo: `${process.env.BASE_PATH || ''}/images/logo.png`, // or use a string for the logo e.g. "TechStartup"
     googleAnalyticsId: '', // e.g. G-XXXXXXX,
-}
+};
